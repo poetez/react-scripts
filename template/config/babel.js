c7a60bf4b12b9acc58bc40fb28efb59120e225d4
Vlad Rindevich <rindevich.vs@gmail.com>
2018-09-17 17:01:38 +0300

    fix(template): use proper definition of babel plugins

--- template/config/babel.js
+++ template/config/babel.js
@@ -4,10 +4,10 @@
 // They are included by default
 module.exports = {
   plugins: [
-    require('@babel/plugin-proposal-object-rest-spread'), {
+    [require('@babel/plugin-proposal-object-rest-spread'), {
       loose: true,
       useBuiltIns: true,
-    },
+    }],
     require('@babel/plugin-proposal-class-properties'),
     require('@babel/plugin-syntax-dynamic-import'),
   ],
