cebe836de1a0d45cd255103d5f8ffba794df2a15
Vlad Rindevich <rindevich.vs@gmail.com>
2018-10-12 23:55:44 +0300

    fix(template): fix some issues & update to CRA v2 template

--- template/src/modules/shared/components/App.tsx
+++ template/src/modules/shared/components/App.tsx
@@ -7,12 +7,19 @@ export default class App extends React.Component {
     return (
       <div className={styles.container}>
         <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo"/>
-          <h1 className={styles.title}>Welcome to React</h1>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className={styles.link}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
-        <p className={styles.logo}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
