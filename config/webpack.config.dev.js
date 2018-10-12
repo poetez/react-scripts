8b43bd7a087248d178794ebf94a2203c0171afcc
Vlad Rindevich <rindevich.vs@gmail.com>
2018-10-13 02:10:55 +0300

    refactor(config): add ".mjs" extension back

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -211,6 +211,7 @@ module.exports = {
       '.json',
       '.web.jsx',
       '.jsx',
+      '.mjs',
     ],
     alias: {
       // @remove-on-eject-begin
@@ -269,7 +270,7 @@ module.exports = {
           },
           // Process application JS with Babel.
           {
-            test: /\.(ts|tsx|js|jsx)$/,
+            test: /\.(ts|tsx|js|jsx|mjs)$/,
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
@@ -298,7 +299,7 @@ module.exports = {
           // Process any JS outside of the app with Babel.
           // Unlike the application JS, we only compile the standard ES features.
           {
-            test: /\.js$/,
+            test: /\.(js|mjs)$/,
             exclude: /@babel(?:\/|\\{1,2})runtime/,
             loader: require.resolve('babel-loader'),
             options: {
@@ -335,7 +336,7 @@ module.exports = {
             // its runtime that would otherwise be processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             loader: require.resolve('file-loader'),
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -269,6 +269,7 @@ module.exports = {
       '.json',
       '.web.jsx',
       '.jsx',
+      '.mjs',
     ],
     alias: {
       // Support React Native Web
@@ -346,7 +347,7 @@ module.exports = {
           // Process any JS outside of the app with Babel.
           // Unlike the application JS, we only compile the standard ES features.
           {
-            test: /\.js$/,
+            test: /\.(js|mjs)$/,
             exclude: /@babel(?:\/|\\{1,2})runtime/,
             loader: require.resolve('babel-loader'),
             options: {
@@ -383,7 +384,7 @@ module.exports = {
             // it's runtime that would otherwise be processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
